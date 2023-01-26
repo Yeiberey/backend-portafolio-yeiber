82c6aa0dcaa18434cf063dd9bb9d310fbff77790
yeiberey <yeiberey@gmail.com>
2023-01-26 16:46:56 -0500

    vercel 2

--- src/app.js
+++ src/app.js
@@ -4,21 +4,12 @@ const express = require("express");
 // const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
-require("./db.js");
+// require("./db.js");
 
 const server = express();
-const cors = require("cors");
+// const cors = require("cors");
 
 server.name = "API";
 server.use("/", routes);
-
-// Error catching endware.
-server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
 
 module.exports = server;
