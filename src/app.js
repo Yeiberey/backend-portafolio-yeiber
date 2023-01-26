20c2cfe04f54a7acebe057aeb2ade1ff27601c93
yeiberey <yeiberey@gmail.com>
2023-01-26 16:45:40 -0500

    vercel1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
 require("./db.js");
@@ -10,26 +10,6 @@ const server = express();
 const cors = require("cors");
 
 server.name = "API";
-
-server.use(cors());
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
-server.use(cookieParser());
-server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-// server.use(
-//   express.json({
-//     limit: "50mb",
-//   })
-// );
 server.use("/", routes);
 
 // Error catching endware.
